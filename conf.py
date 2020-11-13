9bb65eac88240c2339664abdfe6843df012fd675
Gaurav Yadav <47393893+gaurav800@users.noreply.github.com>
2020-11-13 23:11:47 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 EMAIL="--gmail-id--"												#gmail email-id
-PASS="--password--"													#gmail password
+PASS="--password--"								    			#gmail password
 meet="--meet link--"												#meet link (Eg:https://meet.google.com/kvf-ugty-yat. Full path is required 
-min_participants=25 												#number of students below which you wil be disconnected from the meeting
-min_time=600														#approx mininmum time of the meeting in seconds
-target=['gaurav','roll number 9','ece 9','roll number nine','easy 9']#Replace with words for which when called, you would want to exit the meeting
+min_participants=25 								 				#number of students below which you wil be disconnected from the meeting
+min_time=600					    					    		#approx mininmum time of the meeting in seconds
+target=['gaurav','roll number 9','ece 9','roll number nine','easy 9']               #Replace with words for which when called, you would want to exit the meeting
